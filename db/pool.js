8ede421a2003ebd25359285a143574243a852758
LintonRobinson <40444726+LintonRobinson@users.noreply.github.com>
2026-06-20 23:06:53 -0400

    fix: use environment variable for database connection string

--- db/pool.js
+++ db/pool.js
@@ -1,3 +1,3 @@
 const { Pool } = require("pg");
 
-module.exports = new Pool({ connectionString: "postgresql://lintonrobinson@localhost:5432/top_users" });
+module.exports = new Pool({ connectionString: process.env.DATABASE_URL });
